846c84d43e7645242655c64c30381f4fe00d0b8d
NexusI9 <nassim.elkhantour@gmail.com>
2023-06-24 15:45:16 -0400

    updated meta header

--- Webapp/src/pages/_app.js
+++ Webapp/src/pages/_app.js
@@ -31,14 +31,14 @@ export default function App({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="theme-color" content="#000000" />
 
-                <meta name="description" content="KINOJI - Your asian cinema visual library" />
-                <meta property="og:site_name" content="KINOJI - Your asian cinema visual library"/>
-                <meta name="twitter:site_name" content="KINOJI - Your asian cinema visual library"/>
-                <meta content="KINOJI - Your asian cinema visual library" property="og:title"/>
-                <meta content="KINOJI - Your asian cinema visual library" property="twitter:title"/>
-                <meta content="Kinoji is a free online cinematography library gathering thousands shots from hundreds asian movies and anime." name="description"/>
-                <meta content="Kinoji is a free online cinematography library gathering thousands shots from hundreds asian movies and anime." property="og:description"/>
-                <meta content="Kinoji is a free online cinematography library gathering thousands shots from hundreds asian movies and anime." property="twitter:description"/>
+                <meta name="description" content="KINOJI :: Your asian movie stills library" />
+                <meta property="og:site_name" content="KINOJI :: Your asian movie stills library"/>
+                <meta name="twitter:site_name" content="KINOJI :: Your asian movie stills library"/>
+                <meta content="KINOJI :: Your asian movie stills library" property="og:title"/>
+                <meta content="KINOJI :: Your asian movie stills library" property="twitter:title"/>
+                <meta content="Kinoji is the free movie stills library, gathering thousands High-Res shots from hundreds Asian movies and anime." name="description"/>
+                <meta content="Kinoji is the free movie stills library, gathering thousands High-Res shots from hundreds Asian movies and anime." property="og:description"/>
+                <meta content="Kinoji is the free movie stills library, gathering thousands High-Res shots from hundreds Asian movies and anime." property="twitter:description"/>
                 <meta property="og:type" content="website"/>
                 <link rel="manifest" href="/manifest.json" />
             </Head>
