ae6e427377c6fe238dfc4cbb0c0869b8a787095d
NexusI9 <nassim.elkhantour@gmail.com>
2023-06-24 20:14:48 -0400

    work on responsive

--- Webapp/src/components/header/LabelBar.js
+++ Webapp/src/components/header/LabelBar.js
@@ -1,8 +1,29 @@
+import { useEffect, useState, useRef } from "react";
+
 export default ({label, ico , hyperlink, hero=true, underline=true, sticky=false}) => {
 
+    const [stuck, setStuck] = useState(false);
+    const ref = useRef();
+
+    useEffect( () => {
+      
+      const onScroll = () => {
+        const { top } = ref.current.getBoundingClientRect();
+        if(top === 59){ setStuck(true);  }
+        else if(top > 59){ setStuck(false); }
+      }
+
+      if(sticky && ref.current){
+        window.addEventListener('scroll', onScroll);
+      }else{
+        window.removeEventListener('scroll', onScroll);
+      }
+
+      return () => window.removeEventListener('scroll', onScroll);
+    },[ref.current]);
     ico = ico ? <span className={'ico bigico '+ ico}></span> : null;
     return(
-      <div className={`label-bar ${underline ? 'underline' : '' } ${sticky ? 'sticky' : ''}`}>
+      <div ref={ref} className={`label-bar ${underline ? 'underline' : '' } ${sticky ? 'sticky' : ''} ${stuck ? 'stuck' : ''}`}>
       { hero ? <h2 className='label'>{ico}{label}</h2> : <h4 className='label'>{ico}{label}</h4>  }
       { hyperlink }
       </div>
--- Webapp/src/components/home/ListSelection.js
+++ Webapp/src/components/home/ListSelection.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from 'react';
 import { Cta } from '../inputs';
 import { Rubrique } from '../movie';
 import { LabelBar } from '../header';
@@ -8,22 +8,24 @@ import { shuffle_array } from '../../lib/utilities';
 
 export default () => {
 
-    const [ genres, setGenres ] = useState([]);
-    useEffect( () => {
-      const api = useAPI();
-      api.post({type:'GET_COLLECTION', genre:''}).then( result => setGenres(result.data));
-    }, []);
-  
-    return(
+  const [genres, setGenres] = useState([]);
+  useEffect(() => {
+    const api = useAPI();
+    api.post({ type: 'GET_COLLECTION', genre: '' }).then(result => setGenres(result.data));
+  }, []);
+
+  return (
+    <>
       <section className='container'>
-        <LabelBar 
-          label='Collections' 
-          hyperlink= { <Cta href='/collections' type='secondary'>See all collections</Cta> } 
+        <LabelBar
+          label='Collections'
+          hyperlink={<Cta href='/collections' type='secondary'>See all collections</Cta>}
           underline={false}
-          />
-        <div id='rubrique_container'>
-          { shuffle_array(genres).slice(0,4).map( (genre,i) => <Rubrique key={'rubrique_'+i} genre={genre} /> ) }
-        </div>
+        />
       </section>
-    );
-  }
+      <div id='rubrique_container'>
+        {shuffle_array(genres).slice(0, 4).map((genre, i) => <Rubrique key={'rubrique_' + i} genre={genre} />)}
+      </div>
+    </>
+  );
+}
